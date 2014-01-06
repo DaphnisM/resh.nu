c0ae4df1922508fb50f8ddaad909acae6bdef900
Andrew S. Freeman <andrew@andrewsfreeman.com>
2014-01-05 21:17:39 -0800

    also tomorrow...

--- js/main.js
+++ js/main.js
@@ -137,6 +137,8 @@ jQuery(document).ready(function($){
 	var source   = $("#entry-template").html();
 	var template = Handlebars.compile(source);
 	var today = get_date( new Date() );
+	var tomorrow = get_date( new Date() + 1 );
 	var html = template( today );
 	jQuery('body').append( html );
+	jQuery('body').append( template( tomorrow ) );
 });
