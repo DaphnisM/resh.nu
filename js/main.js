10229d9ea8968da82305ab5241e1b32ffb7c94a9
Andrew S. Freeman <andrew@andrewsfreeman.com>
2014-01-19 14:03:14 -0800

    Remove tomorrow, also add thelemic date for resh times.

--- js/main.js
+++ js/main.js
@@ -139,8 +139,8 @@ jQuery(document).ready(function($){
 
 	var source   = $("#entry-template").html();
 	var template = Handlebars.compile(source);
+	Handlebars.registerHelper('get_thelemic_date', get_thelemic_date);
 	var today = get_date( new Date() );
-	var tomorrow = get_date( new Date(new Date().getTime() + 24 * 60 * 60 * 1000) );
 	var html = template( today );
 	jQuery('body').append( html );
 	jQuery('body').append( template( tomorrow ) );
